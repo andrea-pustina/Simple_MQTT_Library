db47acbf6f5b00f5317c4f287d58235455139279
andrea pustina <apustin@gmail.com>
2018-06-12 23:53:52 +0200

    add examples

---
+++ example/client_blocking.py
@@ -0,0 +1,24 @@
+from Mqtt import Mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected")
+    #subscribe here to topics
+
+def on_disconnect(client, userdata, flags):
+    print("Disconnected, should auto reconnect")
+
+def on_message(client, userdata, paho_msg):
+    msg = paho_msg.payload.decode("utf-8")
+    print(msg)
+
+mqtt = Mqtt('server_ip', 8885, 'client_Id')
+mqtt.setAuthentication('user', 'pass')
+mqtt.setSsl()
+mqtt.connect()
+mqtt.setOnConnectCallback(on_connect)
+mqtt.setOnDisconnectCallback(on_disconnect)
+mqtt.setOnMessageCallback(on_message)
+mqtt.loopForever() #blocking (auto reconnect)
+
+print("Here you can do your job")
+
